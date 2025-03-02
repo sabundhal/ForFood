061e372b22f8aa02f750b0b4482e3fea2e459c52
sabundhal <sabundhal@gmail.com>
2025-03-02 16:58:32 +0300

    2 переход на алхимию

--- server/antipyretic_calculator.py
+++ server/antipyretic_calculator.py
@@ -1,86 +1,5 @@
-from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from flask_cors import CORS
-import sqlite3
-import hashlib
-
-
-
-##Расчет дозировки и сохранение в БД@app.route('/calculate', methods=['POST'])
-# @app.route('/api/calculate', methods=['POST'])
-# #@jwt_required()  # Защищаем маршрут JWT-токеном
-# def calculate_dosage():
-#     data = request.json
-#     user_id = data.get('user_id')
-#     drug_id = data.get('drug_id')
-#     weight = data.get('weight')
-#     weight = float(weight)
-#
-#     # Проверяем наличие обязательных параметров
-#     if user_id is None or drug_id is None or weight is None:
-#         return jsonify({'error': 'user_id, drug_id, and weight are required'}), 400
-#     # Проверяем корректность веса
-#     if weight <= 0:
-#         return jsonify({'error': 'Weight must be greater than 0'}), 400
-#     # Получаем данные о препарате из базы данных
-#     try:
-#         conn = sqlite3.connect('myapp.db')
-#         cursor = conn.cursor()
-#
-#         # Запрос данных о препарате
-#         #можно ли тут написать. select *?
-#         cursor.execute('''SELECT name, category_id, mls_var, mgs_var
-#                           FROM drugs
-#                           WHERE id = ?''', (drug_id,))
-#         drug = cursor.fetchone()
-#
-#         if not drug:
-#             conn.close()
-#             return jsonify({'error': 'Drug not found'}), 404
-#
-#         # Извлекаем данные из результата запроса
-#         name, category_id, mls_var, mgs_var = drug
-#
-#         # Выполняем расчеты
-#         mls_total = weight * mls_var
-#         mgs_total = weight * mgs_var
-#
-#         # Сохраняем расчет в историю
-#         # Установка значений для всех 23 полей
-#         cursor.execute('''
-#             INSERT INTO calculation_history (
-#                 user_id, calculation_id, drug_id, drug_name, username, weight, dosage_mls, dosage_mgs,
-#                 totalMgs, totalhigh, totalhighsachets, maximumMgsPerDay, highMgs,
-#                 loading_dose, strep_drug, messageMgs, messageOther, calculation_type,
-#                 calculation_status, calculation_version, patient_id, patient_name, error_message, age
-#             ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)''',
-#                        (
-#                            user_id, None, drug_id, name, 'Username', weight, mls_total, mgs_total,
-#                            0, 0, 0, 0, 0, 0, 0, 'Message', None, category_id,
-#                            'Status', 'Version', 0, 'Patient Name', None , None
-#                        ))
-#         # Получаем автоматически сгенерированный id
-#         calculation_id = cursor.lastrowid
-#         # Обновляем запись, чтобы установить calculation_id равным id
-#         cursor.execute('''
-#             UPDATE calculation_history
-#             SET calculation_id = ?
-#             WHERE id = ?''',
-#                        (calculation_id, calculation_id))
-#         conn.commit()
-#
-#
-#     except sqlite3.Error as e:
-#         return jsonify({'error': f'Database error: {str(e)}'}), 500
-#
-#         # Возвращаем ответ с calculation_id
-#     return jsonify({
-#             'mlsTotal': mls_total,
-#             'mgsTotal': mgs_total,
-#             'calculation_id': calculation_id
-#         })
-#     conn.close()
-
+from data import DatabaseManager, Drug , CalculationHistory
+db_manager = DatabaseManager()
 
 def validateInput(data):
     """
@@ -97,6 +16,7 @@ def validateInput(data):
     except ValueError:
         raise ValueError("Weight must be a valid number")
 
+
 def fetchDrugInfo(drug_id):
     """
     Получает данные о препарате из базы данных.
@@ -105,35 +25,37 @@ def fetchDrugInfo(drug_id):
     :raises ValueError: Если препарат не найден или произошла ошибка БД
     """
     try:
-        conn = sqlite3.connect('myapp.db')
-        cursor = conn.cursor()
-        cursor.execute('''SELECT name, category_id, mls_var, mgs_var, mls_max, mgs_max, high_range, high_modifier, mls_max_high, mgs_max_high, instructions, nzf_link, number_of_times_a_day, range1_dose,range2_dose
-                                 FROM drugs
-                                 WHERE id = ?''', (drug_id,))
-        drug = cursor.fetchone()
+        # Создание сессии
+        session = db_manager.Session()
+
+        # Выполняем запрос
+        drug = session.query(Drug).filter_by(id=drug_id).first()
         if not drug:
             raise ValueError("Drug not found")
+
+        # Возвращаем данные о препарате
         return {
-            'name': drug[0],                 # Название препарата
-            'category_id': drug[1],          # ID категории
-            'mls_var': drug[2],              # Объем на одну дозу (мл)
-            'mgs_var': drug[3],              # Доза на одну дозу (мг)
-            'mls_max': drug[4],              # Максимальный объем в сутки (мл)
-            'mgs_max': drug[5],              # Максимальная доза в сутки (мг)
-            'high_range': drug[6],           # Флаг для высокой дозировки
-            'high_modifier': drug[7],        # Модификатор для высокой дозировки
-            'mls_max_high': drug[8],         # Максимальный объем для высокой дозировки (мл)
-            'mgs_max_high': drug[9],
-            'instructions': drug[10],
-            'nzf_link': drug[11],
-            'number_of_times_a_day': drug[12],
-            'range1_dose': drug[13],
-            'range2_dose': drug[14]
+            'name': drug.name,                 # Название препарата
+            'category_id': drug.category_id,   # ID категории
+            'mls_var': drug.mls_var,          # Объем на одну дозу (мл)
+            'mgs_var': drug.mgs_var,          # Доза на одну дозу (мг)
+            'mls_max': drug.mls_max,          # Максимальный объем в сутки (мл)
+            'mgs_max': drug.mgs_max,          # Максимальная доза в сутки (мг)
+            'high_range': drug.high_range,    # Флаг для высокой дозировки
+            'high_modifier': drug.high_modifier,  # Модификатор для высокой дозировки
+            'mls_max_high': drug.mls_max_high,  # Максимальный объем для высокой дозировки (мл)
+            'mgs_max_high': drug.mgs_max_high,  # Максимальная доза для высокой дозировки (мг)
+            'instructions': drug.instructions,  # Инструкции
+            'nzf_link': drug.nzf_link,        # Ссылка на NZF
+            'number_of_times_a_day': drug.number_of_times_a_day,  # Количество приемов в день
+            'range1_dose': drug.range1_dose,  # Доза для диапазона 1
+            'range2_dose': drug.range2_dose   # Доза для диапазона 2
         }
-    except sqlite3.Error as e:
+
+    except Exception as e:
         raise ValueError(f"Database error: {str(e)}")
     finally:
-        conn.close()
+        session.close()  # Закрываем сессию
 
 def calculateDosage(weight, drug_info):
     """
@@ -212,44 +134,70 @@ def calculateDosage(weight, drug_info):
                 result['high_dose_mg'] = mgs_high
 
     return result
-#def saveCalculationToDB(data, mls_total, mgs_total, drug_info, category_id, mls_high):
-def saveCalculationToDB(data, standard_dose_ml, standard_dose_mg, drug_info, category_id, high_dose_ml,suppositories_min, suppositories_high ):
+
+
+
+def saveCalculationToDB(data, standard_dose_ml, standard_dose_mg, drug_info, category_id, high_dose_ml, suppositories_min, suppositories_high):
     """
     Сохраняет результат расчета в базу данных.
     :param data: Входные данные (user_id, drug_id, weight)
-    :param mls_total: Рассчитанный объем (мл)
-    :param mgs_total: Рассчитанная доза (мг)
+    :param standard_dose_ml: Рассчитанный объем (мл)
+    :param standard_dose_mg: Рассчитанная доза (мг)
     :param drug_info: Данные о препарате
+    :param category_id: ID категории
+    :param high_dose_ml: Высокая доза (мл)
+    :param suppositories_min: Минимальная доза суппозиториев
+    :param suppositories_high: Максимальная доза суппозиториев
     :return: ID расчета
     :raises ValueError: Если произошла ошибка БД
     """
     try:
-        conn = sqlite3.connect('myapp.db')
-        cursor = conn.cursor()
-        cursor.execute('''
-                    INSERT INTO calculation_history (
-                        user_id, calculation_id, drug_id, drug_name, username, weight, dosage_mls, dosage_mgs,
-                        totalMgs, totalhigh, totalhighsachets, maximumMgsPerDay, highMgs,
-                        loading_dose, strep_drug, suppositories_high, suppositories_min, calculation_type,
-                        calculation_status, calculation_version, patient_id, patient_name, error_message, age
-                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)''',
-                               (
-                                   data['user_id'], None, data['drug_id'], drug_info['name'], 'Username', float(data['weight']), standard_dose_ml, standard_dose_mg,
-                                   0, high_dose_ml, 0, 0, 0, 0, 0, suppositories_high, suppositories_min, category_id,
-                                   'Status', 'Version', 0, 'Patient Name', None , None))
-        calculation_id = cursor.lastrowid
-        # Обновляем запись, чтобы установить calculation_id равным id
-        cursor.execute('''
-                    UPDATE calculation_history
-                    SET calculation_id = ?
-                    WHERE id = ?''',
-                               (calculation_id, calculation_id))
-        conn.commit()
-        return calculation_id
-    except sqlite3.Error as e:
+        # Создание сессии
+        session = db_manager.Session()
+
+        # Создаем объект CalculationHistory
+        calculation = CalculationHistory(
+            user_id=data['user_id'],
+            calculation_id=None,  # Пока оставляем None
+            drug_id=data['drug_id'],
+            drug_name=drug_info['name'],
+            username='Username',  # Заглушка, можно заменить на реальное имя пользователя
+            weight=float(data['weight']),
+            dosage_mls=standard_dose_ml,
+            dosage_mgs=standard_dose_mg,
+            totalMgs=0,  # Заглушка
+            totalhigh=high_dose_ml,
+            totalhighsachets=0,  # Заглушка
+            maximumMgsPerDay=0,  # Заглушка
+            highMgs=0,  # Заглушка
+            loading_dose=0,  # Заглушка
+            strep_drug=0,  # Заглушка
+            suppositories_high=suppositories_high,
+            suppositories_min=suppositories_min,
+            calculation_type=category_id,
+            calculation_status='Status',  # Заглушка
+            calculation_version='Version',  # Заглушка
+            patient_id=0,  # Заглушка
+            patient_name='Patient Name',  # Заглушка
+            error_message=None,
+            age=None
+        )
+
+        # Добавляем объект в сессию
+        session.add(calculation)
+        session.commit()
+
+        # Обновляем calculation_id
+        calculation.calculation_id = calculation.id
+        session.commit()
+
+        return calculation.id
+
+    except Exception as e:
+        session.rollback()
         raise ValueError(f"Database error: {str(e)}")
     finally:
-        conn.close()
+        session.close()  # Закрываем сессию
 
 
 def calculateAntipyreticDosage(data):
